474462fcf71a2813c1331bedf51616978a2c0cf1
Cássio Cardoso <59260142+cassiocardoso89@users.noreply.github.com>
2023-04-15 00:12:14 -0300

    correções e novas features
    correções nas listagens de materias;
    professor cadastra evento;
    o aluno lista apenas as materias com conteudo cadastrado;
    aluno lista automaticamente eventos cadastrados

--- html/aluno/index.php
+++ html/aluno/index.php
@@ -1,29 +1,25 @@
 <?php
 session_start();
-include('../../php/lista_materias.php');
-
-include('../../php/lista_conteudo.php');
+include('../../php/lista_materias_aluno.php');
+if (isset($_GET['materia'])) {
+    include('../../php/lista_conteudo.php');
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../css/aluno.css">
     <link rel="stylesheet" href="../../css/geral.css">
-    <title>
-        <?php echo $_SESSION['usuario']; ?>
-    </title>
-</head>
 
+    <title><?php echo $_SESSION['usuario']; ?></title>
+</head>
 <body>
     <header>
-        <h1>
-            <?php echo 'Bem-vindo, ' . $_SESSION['usuario'] . '!'; ?>
-        </h1>
+        <h1><?php echo 'Bem-vindo, ' . $_SESSION['usuario'] . '!'; ?></h1>
         <section>
             <form action=# method="GET">
                 <select name="materia" id="materia">
@@ -35,19 +31,24 @@ include('../../php/lista_conteudo.php');
             </form>
         </section>
     </header>
-    <main>
-        <?php
-        while ($conteudos = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            echo '<h3>' . $conteudos['titulo'] . '</h3>';
-            echo '<p>Professor: ' . $conteudos['nome'] . ' | '  . $conteudos['materia'] . '</p>';
-            echo '<p>Descrição: ' . $conteudos['conteudo'] . '</p>';
-            echo '<hr>';
+    <main>        <?php
+        if (isset($_GET['materia'])) {
+            while ($conteudos = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                echo '<h3>' . $conteudos['titulo'] . '</h3>';
+                echo '<p>Professor: ' . $conteudos['nome'] . ' | '  . $conteudos['materia'] . '</p>';
+                echo '<p>Descrição: ' . $conteudos['conteudo'] . '</p>';
+                echo '<hr>';
+            }
+        } else {
+            echo 'Selecione uma matéria para exibir os conteúdos.';
         }
         ?>
-    </main>
+        </main>
 
+        <section class="eventos">
+        <?php
+            include('../../php/lista_eventos.php');
+        ?>
+        </section>
 </body>
-
 </html>
-
-
